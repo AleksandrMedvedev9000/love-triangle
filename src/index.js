3bfb994e4c518478a76d5c703a4b1b24bd8e9be1
Miadzvedzeu <amiadzvedzeu@scnsoft.com>
2018-09-26 10:22:35 +0300

    smile

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  k = 0;
+  var i, j, l;
+  for (i=0; i<preferences.length; i++) {
+    for (j=0; j<preferences.length; j++) {
+      if (preferences[i] == j+1 && preferences[j] != preferences[i]) {
+        for (l=0; l<preferences.length; l++) {
+          if (preferences[j] == l+1 && preferences[l] == i+1 && preferences[j] != preferences[l]) {
+            k = k+1;
+            }
+        }
+      }
+    }
+  }
+  return k/3;
 };
